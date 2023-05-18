c1ed49d54e6f9eee1884d6e76c7939b86aa53fcc
Matheus-Almeida21 <matheus.cavallari7@gmail.com>
2023-05-18 12:41:51 -0300

    Criando custom control e estilizando botões Prev e Next

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ import {Slide, SlideNav} from './slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next')
-slide.addControl();
+slide.addControl('.custom-control');
 console.log(slide);
